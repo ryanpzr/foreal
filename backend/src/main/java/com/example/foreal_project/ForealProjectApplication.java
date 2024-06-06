d80d07d88f8bf8c9ec5822059b07fad4441f6a02
ryanpzr <ryanpereiralimads@gmail.com>
2024-06-05 21:03:52 -0300

    first commit

---
+++ backend/src/main/java/com/example/foreal_project/ForealProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.foreal_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForealProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForealProjectApplication.class, args);
+	}
+
+}
