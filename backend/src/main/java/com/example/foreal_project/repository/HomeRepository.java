eaf06a8d6aada0fc0e5689405d07b06388373af6
ryanpzr <ryanpereiralimads@gmail.com>
2024-06-07 20:33:11 -0300

    Update classes and properties

---
+++ backend/src/main/java/com/example/foreal_project/repository/HomeRepository.java
@@ -0,0 +1,9 @@
+package com.example.foreal_project.repository;
+
+import com.example.foreal_project.model.Home;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HomeRepository extends JpaRepository<Home, Long> {
+
+
+}
