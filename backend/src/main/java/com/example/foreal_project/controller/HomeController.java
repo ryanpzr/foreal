28a5d6bec3cd225dad3da80824b1e682c9b0006b
ryanpzr <ryanpereiralimads@gmail.com>
2024-06-07 21:10:21 -0300

    method in service and req @Get created

--- backend/src/main/java/com/example/foreal_project/controller/HomeController.java
+++ backend/src/main/java/com/example/foreal_project/controller/HomeController.java
@@ -1,12 +1,28 @@
 package com.example.foreal_project.controller;
 
+import com.example.foreal_project.dto.HomeDto;
+import com.example.foreal_project.model.Home;
+import com.example.foreal_project.service.HomeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/auth/home")
 public class HomeController {
 
+    @Autowired
+    private HomeService service;
 
+    @GetMapping("/buscarDadosHome")
+    public ResponseEntity<List<Home>> buscarDados() {
+        List<Home> data = service.buscarDados();
+        return ResponseEntity.ok().body(data);
+    }
 
 }
--- backend/src/main/java/com/example/foreal_project/repository/HomeRepository.java
+++ backend/src/main/java/com/example/foreal_project/repository/HomeRepository.java
@@ -2,8 +2,11 @@ package com.example.foreal_project.repository;
 
 import com.example.foreal_project.model.Home;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Component;
 
+import java.util.List;
+@Component
 public interface HomeRepository extends JpaRepository<Home, Long> {
-
 
 }
