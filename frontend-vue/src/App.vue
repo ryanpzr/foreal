00edf493a9c8b7dd2ae773b6fde48cad65b97418
Ryan <ryanpereiralimads@gmail.com>
2024-10-08 22:30:59 -0300

    First Commit Frontend-Vue

---
+++ frontend-vue/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <Navbar />
+    <MainPage />
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+import MainPage from './components/MainPage.vue';
+
+export default {
+  components: {
+      Navbar,
+      MainPage
+  }
+}
+
+</script>
+
+<style>
+
+body {
+    overflow-x: hidden;
+    margin: 0; 
+    padding: 0; 
+    background-color: black;
+}
+
+</style>
